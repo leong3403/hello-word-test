0fb1126dc7ffaa2b58faeafd913dde52677bf4ed
leong3403 <84886677+leong3403@users.noreply.github.com>
2023-08-26 09:46:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   let ra = getRandomInt(100000);
-  const msg = `Hello Node(${ra})!\n`
+  const msg = `Hello Node 2(${ra})!\n`
   res.end(msg);
 });
 
