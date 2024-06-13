846bd8608099067a8baa82559c23bb2cb2f7ba80
leo <leo>
2024-06-13 03:58:29 +0000

    update

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 
-let server = getRandomInt(100);
+let serverid = getRandomInt(100);
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   let ra = getRandomInt(100000);
-  const msg = `Hello Node, by server ${server} , (${ra})!\n`
+  const msg = `Hello Node, by server ${serverid} , (${ra})!\n`
   res.end(msg);
 });
 
